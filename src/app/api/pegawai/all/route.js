ba5f8130464887185292d8b1434fa6c2879da910
miqbalfr12 <miqbalfr12@gmail.com>
2024-06-25 10:11:00 +0700

    pindahin

--- src/app/api/pegawai/all/route.js
+++ src/app/api/pegawai/all/route.js
@@ -2,7 +2,6 @@ import {retrieveData} from "@/services";
 
 export async function GET() {
  const data = await retrieveData("pegawai");
- console.log(data);
  const ResponseInit = {status: 200, statusText: "OK"};
  return Response.json({...ResponseInit, message: data});
 }
