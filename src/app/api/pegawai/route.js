2c2480328ec6d5512316051bb400aeb9882dc4a5
miqbalfr12 <miqbalfr12@gmail.com>
2024-06-25 10:18:12 +0700

    Cache-Control no-store

--- src/app/api/pegawai/route.js
+++ src/app/api/pegawai/route.js
@@ -2,6 +2,12 @@ import {retrieveData} from "@/services";
 
 export async function GET() {
  const data = await retrieveData("pegawai");
- const ResponseInit = {status: 200, statusText: "OK"};
+ const ResponseInit = {
+  status: 200,
+  statusText: "OK",
+  headers: {
+   "Cache-Control": "no-store",
+  },
+ };
  return Response.json({...ResponseInit, message: data});
 }
